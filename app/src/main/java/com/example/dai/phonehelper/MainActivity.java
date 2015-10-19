7c74a4b37df42a402c8ac7d0ac6cd1ee5ae61c6d
Zoe Dai <zoedai91@gmail.com>
2015-10-18 18:10:44 -0700

    open browser added

--- app/src/main/java/com/example/dai/phonehelper/MainActivity.java
+++ app/src/main/java/com/example/dai/phonehelper/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.dai.phonehelper;
 
 import android.app.Notification;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -29,6 +30,44 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 startActivity(new Intent(Intent.ACTION_DIAL));
+            }
+        });
+
+        browserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setComponent(new
+                        ComponentName("com.android.browser", "com.android.browser.BrowserActivity"));
+                startActivity(intent);
+            }
+        });
+
+        calBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CalendarActivity.class));
+            }
+        });
+
+        addrBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddressActivity.class));
+            }
+        });
+
+        locBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LocationActivity.class));
+            }
+        });
+
+        smsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SmsActivity.class));
             }
         });
     }
